988699f4b8bb9be46cb9f74c07838d5db0a49b67
Júlia Campos <160678925+juliliaasc@users.noreply.github.com>
2024-08-19 16:39:14 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/views/welcome.blade.php",
+    "./resources/views/admin/users/create.blade.php",
+    "./resources/views/auth/login.blade.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
